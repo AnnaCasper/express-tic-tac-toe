55382358256aa5bbd82b3fd347c011586d8d9569
Andrew Cer <cer.andrew@gmail.com>
2015-07-10 17:25:15 -0600

    fixed vertical check problem

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -3,68 +3,60 @@ var containerArray = container[0].childNodes
 var counterArray = [];
 var xArray = [];
 var oArray = [];
-// var winningArray = [
-//   ['one', 'two', 'three'],
-//   ['one', 'four', 'seven'],
-//   ['two', 'five', 'eight'],
-//   ['three', 'six', 'nine'],
-//   ['four', 'five', 'six'],
-//   ['seven', 'eight', 'nine'],
-//   ['one', 'five', 'nine'],
-//   ['three', 'five', 'seven']
-// ]
 var gameState = [
   ['one', 'two', 'three'],
   ['four', 'five', 'six'],
   ['seven', 'eight', 'nine']
-]
+];
 
-
+var youWin = function () {
+  if (confirm('You Win! Click "OK" to reset game')) {
+    console.log('do something in here to reset game');
+  }
+}
 
 var winCheck = function (inputArray) {
-
+  var newArray = [[], [], []];
+  //check all horizontals
   for (var i = 0; i < inputArray.length; i++) {
     var inputString = inputArray[i].join(',');
     if (inputString === 'x,x,x' || inputString === 'o,o,o') {
-      alert('You Win!');
-    };
-
-    var newArray = [[], [], []];
+      youWin();
+    }
+    //check all verticalls...currently broken
     for (var j = 0; j < inputArray[i].length; j++) {
       newArray[j].push(inputArray[i][j]);
       var verticalString = newArray[j].join(',');
       if (verticalString === 'x,x,x' || verticalString === 'o,o,o') {
-        alert('You Win!');
-      };
-    };
+        youWin();
+      }
+    }
   }
+  //check left to right diagonal
   var anotherArray = [];
   for (var k = 0; k < inputArray.length; k++) {
     anotherArray.push(inputArray[k][k]);
     var diagonalString = anotherArray.join(',');
     if (diagonalString === 'x,x,x' || diagonalString === 'o,o,o') {
-      alert('You Win!');
-    };
-  }
-
-  var diagonalArray = [];
-  //for (var k = inputArray.length - 1; k >= 0; k--) {
-    diagonalArray.push(inputArray[0][2]);
-    diagonalArray.push(inputArray[1][1]);
-    diagonalArray.push(inputArray[2][0]);
-    // console.log(inputArray[i][k]);
-    // diagonalArray.push(inputArray[i][k]);
-    //}
-    var anotherString = diagonalArray.join(',');
-    if (anotherString === 'x,x,x' || anotherString === 'o,o,o') {
-      alert('You Win!')
+      youWin();
     }
+  }
+  //check right to left diagonal
+  var diagonalArray = [];
+  //hard coded, couldn't get for loop to work
+  diagonalArray.push(inputArray[0][2]);
+  diagonalArray.push(inputArray[1][1]);
+  diagonalArray.push(inputArray[2][0]);
+  var anotherString = diagonalArray.join(',');
+  if (anotherString === 'x,x,x' || anotherString === 'o,o,o') {
+    youWin();
+  }
 }
 
 for (var i = 0; i < containerArray.length; i++) {
   containerArray[i].addEventListener('click', function(){
     counterArray.push(this.id);
-    // console.log(counterArray);
+    //if number is even, it's x's turn, push that into the gameState array
     if(counterArray.length % 2 != 0){
       for (var k = 0; k < gameState.length; k++) {
         for (var j = 0; j < gameState[k].length; j++) {
@@ -86,8 +78,6 @@ for (var i = 0; i < containerArray.length; i++) {
       oArray.push(this.id);
       this.firstChild.src = '../images/giant-o.gif';
     }
-    //run funk
     winCheck(gameState);
   });
 };
-//array
